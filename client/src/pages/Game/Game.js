589c0a48cdfebe431921ef3786ba45f9aea01b05
ikevinws <kevinwang1036@gmail.com>
2021-01-26 14:35:20 -0500

    redirect player if player didnt submit join form

--- client/src/pages/Game/Game.js
+++ client/src/pages/Game/Game.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { useGameContext } from '../../contexts/GameContext';
 
 const Game = () => {
+    const { gameState } = useGameContext();
+
+    //redirect if user didn't submit create or join form
+    if (!gameState._id) {
+        return <Redirect to="/" />;
+    }
     return (
         <section className="mt-5 p-5">
             <div>Game Page</div>
