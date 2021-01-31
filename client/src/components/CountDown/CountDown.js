3e9e395785a1ee2ced0035dd95fac1e474b0c0b6
ikevinws <kevinwang1036@gmail.com>
2021-01-30 20:32:43 -0500

    reset countdown after game is reset

--- client/src/components/CountDown/CountDown.js
+++ client/src/components/CountDown/CountDown.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import socket from '../../config/socket';
 
-const CountDown = () => {
-    const [timer, setTimer] = useState({ countDown: 'N/A', message: 'Countdown:' });
+const initaialTimerState = { countDown: 'N/A', message: 'Countdown:' };
+
+const CountDown = ({ isGameOver }) => {
+    const [timer, setTimer] = useState(initaialTimerState);
+
     useEffect(() => {
-        socket.on('timer', (data) => {
-            setTimer(data);
-        });
-        socket.on('done', () => {
-            socket.removeListener('timer');
-        });
+        if (!isGameOver) {
+            socket.on('timer', (data) => {
+                setTimer(data);
+            });
+            socket.on('done', () => {
+                socket.removeListener('timer');
+            });
+            //reset timer
+            setTimer(initaialTimerState);
+        }
+        //if game is over remove listeners and reset sockets
         return () => {
             socket.removeListener('timer');
             socket.removeListener('done');
         };
-    }, []);
+    }, [isGameOver]);
 
     return (
         <>
